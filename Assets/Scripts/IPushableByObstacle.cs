bc41b4f9f72643c99edb25a8747c463bb2f5ecca
Dmitrii <malinkin20001@gmail.com>
2025-12-05 21:25:36 +0100

    add character script

--- Assets/Scripts/IPushableByObstacle.cs
+++ Assets/Scripts/IPushableByObstacle.cs
@@ -4,11 +4,3 @@ public interface IPushableByObstacle
 {
     public void Push(Vector2 direction, float force);
 }
-
-public class Character : MonoBehaviour, IPushableByObstacle
-{
-    public void Push(Vector2 direction, float force)
-    {
-        // If you need it do it yourself
-    }
-}
