fde94cbaaaad13520efb24724108830631186cd1
Dmitriy-devv <malinkin20001@gmail.com>
2025-12-06 15:54:41 +0100

    add sprites to chain/bulava

---
+++ Assets/Scripts/Character/ChainRotator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class ChainRotator : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    
+    private void LateUpdate()
+    {
+        var dir = (_target.position - transform.position).normalized;
+        var targetRotation = Quaternion.FromToRotation(Vector2.right, dir);
+        transform.rotation = targetRotation;
+    }
+}
