2ebdb07a68cdca65b17e715db111100a1a89d519
tniets <artem.bernshteyn@gmail.com>
2025-12-06 15:06:50 +0300

    move obstacle component to bulava from collider so stickable object would also count as one

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider2D))]
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] private bool onlyHorizontalPush = true;
