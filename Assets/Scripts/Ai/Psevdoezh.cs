12bb57177087f25d98905855574fef9a09262a85
Gusev Anton Alexandrovich <wan7ed.phenov@yandex.ru>
2025-12-06 22:19:38 +0300

    patrol psevdoezh setup

---
+++ Assets/Scripts/Ai/Psevdoezh.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Psevdoezh : MonoBehaviour
+{
+    [SerializeField] private GameObject _iglaPrefab;
+    [SerializeField] private List<Transform> _shootTransforms;
+    [SerializeField] private float _zalpsInBurst = 3f; 
+    [SerializeField] private float _zalpCooldown = 1.5f;
+    [SerializeField] private float _burstCooldown = 5f;
+
+    private float prevBurstTimeStamp;
+
+    private void Awake()
+    {
+        prevBurstTimeStamp = Time.timeSinceLevelLoad;
+    }
+
+    private void Update()
+    {
+        if (Time.timeSinceLevelLoad - prevBurstTimeStamp > _burstCooldown)
+        {
+            for (var i = 0; i < _zalpsInBurst - 1; i++)
+            {
+                Invoke("ShootZalp", i * _zalpCooldown);
+            }
+
+            Invoke("ShootEndZalp", (_zalpsInBurst - 1) * _zalpCooldown);
+            prevBurstTimeStamp = Time.timeSinceLevelLoad;
+        }
+    }
+
+    private void ShootZalp()
+    {
+        foreach (Transform trs in _shootTransforms)
+        {
+            var igla = Instantiate(_iglaPrefab, trs.position, trs.rotation);
+        }
+    }
+
+    private void ShootEndZalp()
+    {
+        ShootZalp();
+    }
+}
