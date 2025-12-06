2a26be4a0ec67bdfeaf315063643e4aad98b3df4
Kirill Morozov <Tenelles@yandex.ru>
2025-12-06 05:23:31 +0300

    Update CameraMovement.cs

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,13 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] private Player player;
+    [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset;
-    [SerializeField] private AnimationCurve cameraDelayByDistance;
     [SerializeField] List<CameraBoundaries> boundaries;
     
     private Camera _camera;
@@ -19,26 +17,64 @@ public class CameraMovement : MonoBehaviour
 
     private void LateUpdate()
     {
-        var bottomLeft = _camera.ScreenToWorldPoint(Vector3.zero);
-        var topRight = _camera.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight));
-        var cameraRect = new Rect(bottomLeft.x, bottomLeft.y, topRight.x - bottomLeft.x, topRight.y - bottomLeft.y);
-        var cameraWidth = Mathf.Abs(bottomLeft.x - topRight.x);
-        var playerPosition = player.transform.position;
-        var boundary = boundaries[0];
-        for (var i = 0; i < boundaries.Count; i++)
-        {
-            if (boundaries[i].Height < playerPosition.y)
-            {
-                boundary = boundaries[i];
-                break;
-            }
-        }    
+        var cameraRect = GetCameraRect();
+        var targetPosition = target.transform.position;
+        var currentBoundary = GetCurrentCoundaries(targetPosition.y - cameraRect.height / 2f);
 
-        playerPosition.x = Mathf.Clamp(playerPosition.x, boundary.LeftBound + cameraWidth / 2f, boundary.RightBound - cameraWidth / 2f);
-        playerPosition.z = transform.position.z;
-        playerPosition += offset;
-        transform.position = playerPosition;
+        targetPosition.x = Mathf.Clamp(targetPosition.x, currentBoundary.LeftBound + cameraRect.width / 2f, currentBoundary.RightBound - cameraRect.width / 2f);
+        targetPosition.z = transform.position.z;
+        targetPosition += offset;
+        
+        transform.position = targetPosition;
     }
 
+    private Rect GetCameraRect()
+    {
+        var bottomLeft = _camera.ScreenToWorldPoint(Vector3.zero);
+        var topRight = _camera.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight));
+        return new Rect(bottomLeft.x, bottomLeft.y, topRight.x - bottomLeft.x, topRight.y - bottomLeft.y);
+    }
 
+    private CameraBoundaries GetCurrentCoundaries(float currentY)
+    {
+        if (boundaries[0].Height < currentY)
+            return boundaries[0];
+
+        for (var i = 0; i < boundaries.Count; i++)
+        {
+            if (boundaries[i].Height < currentY)
+                return boundaries[i];
+        }
+
+        return boundaries[^1];
+    }
+
+    [ExecuteAlways]
+    private void OnDrawGizmos()
+    {
+        var boundary = boundaries[0];
+        var left = new Vector3(boundary.LeftBound, boundary.Height, 0);
+        var right = new Vector3(boundary.RightBound, boundary.Height, 0);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(left, Vector3.up * 100f);
+        Gizmos.DrawRay(right, Vector3.up * 100f);
+        for (var i = 1; i < boundaries.Count; i++)
+        {
+            var leftTop = new Vector3(boundaries[i].LeftBound, boundaries[i - 1].Height, 0);
+            var rightTop = new Vector3(boundaries[i].RightBound, boundaries[i - 1].Height, 0);
+
+            var leftBottom = new Vector3(boundaries[i].LeftBound, boundaries[i].Height, 0);
+            var rightBottom = new Vector3(boundaries[i].RightBound, boundaries[i].Height, 0);
+
+            Gizmos.DrawLine(leftTop, leftBottom);
+            Gizmos.DrawLine(rightTop, rightBottom);
+        }
+
+        boundary = boundaries[^1];
+        left = new Vector3(boundary.LeftBound, boundary.Height, 0);
+        right = new Vector3(boundary.RightBound, boundary.Height, 0);
+        Gizmos.DrawRay(left, Vector3.down * 100f);
+        Gizmos.DrawRay(right, Vector3.down * 100f);
+    }
 }
