8474c5f87a12c6d78f8ec891a0e85ee0bdb92b1a
Gusev Anton Alexandrovich <wan7ed.phenov@yandex.ru>
2025-12-06 14:42:45 +0300

    cleanup dbg h*lers / remove serialize fields for patrol points

--- Assets/Scripts/Ai/PatrolEnemy.cs
+++ Assets/Scripts/Ai/PatrolEnemy.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class PatrolEnemy : MonoBehaviour, IPushableByObstacle
 {
     [SerializeField] private float _patrolSpeed = 1f;
-    [SerializeField] private List<Transform> _patrolPoints;
-    
+    [SerializeReference] private GameObject setupParent;
+
+    private void Awake()
+    {
+        var setupChilds = setupParent.GetComponentsInChildren<Transform>();
+
+        foreach (var setupChild in setupChilds)
+        {
+            if (setupChild.name.StartsWith("patrolPoint"))
+            {
+                _patrolPoints.Add(setupChild.transform);
+            }
+        }
+    }
+
     public void Push(Vector2 direction, float force)
     {
         Debug.Log("patrol pushed");
@@ -17,7 +30,7 @@ public class PatrolEnemy : MonoBehaviour, IPushableByObstacle
 
         if (rb != null)
         {
-            rb.AddForce(direction * force);
+            rb.AddForce(direction * force, ForceMode2D.Impulse);
             rb.bodyType = RigidbodyType2D.Dynamic;
         }
     }
@@ -44,6 +57,8 @@ public class PatrolEnemy : MonoBehaviour, IPushableByObstacle
 
     // private
 
+    private List<Transform> _patrolPoints = new List<Transform>();
     private int _currentPointId = 0;
+
     private bool _isPushed = false;
 }
