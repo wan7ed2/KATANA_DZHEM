ca67b59f08f01650b7cae41c28b9383389c6f2df
Gusev Anton Alexandrovich <wan7ed.phenov@yandex.ru>
2025-12-06 06:23:33 +0300

    pushable patrol

--- Assets/Scripts/Ai/AiBehaviour.cs
+++ Assets/Scripts/Ai/AiBehaviour.cs
@@ -2,13 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AiBehaviour : MonoBehaviour
+public class AiBehaviour : MonoBehaviour, IPushableByObstacle
 {
     [SerializeField] private float _patrolSpeed = 1f;
     [SerializeField] private List<Transform> _patrolPoints;
+    
+    public void Push(Vector2 direction, float force)
+    {
+        Debug.Log("patrol pushed");
+        _isPushed = true;
+
+        Rigidbody2D rb;
+        TryGetComponent<Rigidbody2D>(out rb);
+
+        if (rb != null)
+        {
+            rb.AddForce(direction * force);
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+
+        Invoke("ResetPush", 2f);
+    }
 
     private void Update()
     {
+        if (_isPushed)
+            return;
+
         if (_patrolPoints.Count == 0)
             return;
 
@@ -20,11 +40,25 @@ public class AiBehaviour : MonoBehaviour
         var dir = (_patrolPoints[_currentPointId].position - transform.position).normalized;
         var maxStep = (_patrolPoints[_currentPointId].position - transform.position).magnitude;
         var step = Mathf.Clamp(_patrolSpeed * Time.deltaTime, 0, maxStep);
-         
+
         transform.Translate(dir * step);
     }
 
     // private
 
     private int _currentPointId = 0;
+    private bool _isPushed = false;
+
+    private void ResetPush()
+    {
+        _isPushed = false;
+
+        Rigidbody2D rb;
+        TryGetComponent<Rigidbody2D>(out rb);
+
+        if (rb != null)
+        {
+            rb.bodyType = RigidbodyType2D.Kinematic;
+        }
+    }
 }
