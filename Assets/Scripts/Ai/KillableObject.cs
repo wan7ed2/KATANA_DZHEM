d7ff8107533a70c21cccd5435d042b0e14344f84
Dmitriy-devv <malinkin20001@gmail.com>
2025-12-07 16:12:10 +0100

    add game end flow

---
+++ Assets/Scripts/Ai/KillableObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class KillableObject : MonoBehaviour, IKillable
+{
+    [SerializeField] private Collider2D _collider;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    
+    public void Kill()
+    {
+        _collider.enabled = false;
+        _rigidbody.isKinematic = false;
+        Destroy(gameObject, 5f);
+    }
+}
