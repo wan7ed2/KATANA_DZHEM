56bb1344a3b04a0ec1336407e78ef4320259de86
Dmitriy-devv <malinkin20001@gmail.com>
2025-12-07 05:43:59 +0100

    add intro cinematic

---
+++ Assets/Scripts/Meta/IntroCinmaticController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class IntroCinmaticController : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private GameObject _game;
+    [SerializeField] private Animator _introAnimator;
+    
+    private void Start()
+    {
+        _introAnimator.Play("Intro");
+    }
+
+    public void StartGame()
+    {
+        gameObject.SetActive(false);
+
+        var isEvil = PlayerPrefs.GetInt("IsEvil", 0);
+        _player.SetEvil(isEvil != 0);
+        _game.SetActive(true);
+    }
+}
